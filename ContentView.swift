b7c179bd11801522928a0999b066a88d23413f3f
Sam Gebhardt <sgebhardt6133@stu.d214.org>
2023-04-14 13:07:47 -0500

    number textfield

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var Int1 = 0
+    @State var Int2 = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("number", value: $Int1, format: .number)
+            TextField("number", value: $Int2, format: .number)
+            
         }
     }
 }
